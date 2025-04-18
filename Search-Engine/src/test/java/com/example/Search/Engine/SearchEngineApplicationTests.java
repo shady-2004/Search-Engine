0692403b594800e7f2f856c089f87df781834a1a
shady-2004 <shadyshiko2004@gmail.com>
2025-04-18 17:58:49 +0200

    Spring boot Env. initialization

---
+++ Search-Engine/src/test/java/com/example/Search/Engine/SearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Search.Engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
